b39a48cf94600a3c4b16563313546ad76c5dc0d7
Roy Van Liew <vanliewroy@gmail.com>
2014-03-29 13:11:09 -0400

    Priority Queue Done

--- HW6_STL/HW6_STL.cpp
+++ HW6_STL/HW6_STL.cpp
@@ -2,7 +2,7 @@
  * File:   HW6_STL.cpp
  * Author: Roy Van Liew and Saqib Zahid
  *
- * Last Modified on March 25, 2014, 12:55 PM
+ * Last Modified on March 29, 2014, 1:09 PM
  */
 
 #include <iostream>
@@ -11,6 +11,7 @@
 #include <string>
 #include <stack> // C++ library for stack STL
 #include <map> // C++ library for map STL
+#include <queue> // C++ library for queue and priority queue STLs
 #include <vector> // C++ library for vectors
 
 using namespace std;
@@ -25,6 +26,7 @@ class Matrix
                 ~Matrix(); // Destructor
                 friend ostream& operator <<(ostream& out, const Matrix& Mat);
                 friend bool operator ==(const Matrix& Mat1, const Matrix& Mat2);
+                friend bool operator <(const Matrix& Mat1, const Matrix& Mat2);
         private:
                 const static int n=2; // To reduce clutter we just have 2x2 matrices.
                 int data[n][n];
@@ -67,6 +69,20 @@ bool operator ==(const Matrix& Mat1, const Matrix& Mat2){
         return true;
     
 }
+// Overloaded less than operator
+bool operator <(const Matrix& Mat1, const Matrix& Mat2){
+    
+        int i, j;
+        for (i = 0; i < Mat1.n ; i++) {
+            for (j = 0; j < Mat1.n ; j++) {
+                if (Mat1.data[i][j] < Mat2.data[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    
+}
 /*
  * END DEFINING CLASS MATRIX.
  */
@@ -76,7 +92,7 @@ bool operator ==(const Matrix& Mat1, const Matrix& Mat2){
  stackVar is used as a general type here. The type is dependent on what's passed to the function
  *********************/
 template<class stackVar>
-void stackTest(stackVar& A, stackVar& B) {
+void stackTest(stackVar& A, stackVar& B, stackVar& C) {
     
     cout << "========== STACK STL TESTING ==========\n" << endl;
     
@@ -85,8 +101,10 @@ void stackTest(stackVar& A, stackVar& B) {
     cout << "Pushing to stack:" << endl;
     cout << A << endl;
     cout << B << endl;
+    cout << C << endl;
     stack1.push(A);
     stack1.push(B);
+    stack1.push(C);
     
     // Using copy constructor from stack STL.
     stack<stackVar> stack2(stack1);
@@ -119,7 +137,7 @@ void stackTest(stackVar& A, stackVar& B) {
  mapVar is used as a general type here. The type is dependent on what's passed to the function
  *********************/
 template<class mapVar>
-void mapTest(mapVar& A, mapVar& B) {
+void mapTest(mapVar& A, mapVar& B, mapVar& C) {
     
     cout << "========== MAP STL TESTING ==========\n" << endl;
     
@@ -127,8 +145,10 @@ void mapTest(mapVar& A, mapVar& B) {
     map<const char*, mapVar> map1;
     map1["one"] = A;
     map1["two"] = B;
+    map1["three"] = C;
     cout << "There are " << map1.count("one") << " keys \"one\" with value:\n" << map1["one"] << endl;
     cout << "There are " << map1.count("two") << " keys \"two\" with value:\n" << map1["two"] << endl;
+    cout << "There are " << map1.count("three") << " keys \"three\" with value:\n" << map1["three"] << endl;
     
     cout << "\nMaking a second map as a copy of the first map." << endl;
     map<const char*, mapVar> map2(map1);
@@ -136,17 +156,16 @@ void mapTest(mapVar& A, mapVar& B) {
     if( map1 == map2 )
         cout << "Both maps are the same." << endl;
     
-    // This is for reinitializing the maps
+    // This is for saving original map to reinitialize after modification
     map<const char*, mapVar> tempMap = map2;
     
-    // Erase the same key in both maps.
-    cout << "Removing the key \"one\" that had the following element in both maps:\n" << map1["one"] << endl;
+    // Erase the same key in both maps. In this case, it was in order.
+    cout << "Removing the key \"one\" with this value in both maps:\n" << map1["one"] << endl;
     map1.erase( map1.begin() );
     map2.erase( "one" );
     
     if( map1 == map2 )
         cout << "\nElement removed in both maps, both maps are the same.\n" << endl;
-    
     
     tempMap.swap( map1 );
     if( tempMap == map2 )
@@ -154,12 +173,49 @@ void mapTest(mapVar& A, mapVar& B) {
     
     cout << "Map element \"two\" of first map is:\n" << map1["two"] << "\n" << endl;
     
-    cout << "The size of the first map is " << map1.size() << "." << endl;
+    cout << "The size of the first map is " << map1.size() << " after swap." << endl;
     
     map2.clear();
     if( map2.empty() )
         cout << "Emptied the second map.\n" << endl;
     
+}
+
+/*********************
+ GENERALIZED PRIORITY QUEUE STL FUNCTION
+ pqVar is used as a general type here. The type is dependent on what's passed to the function
+ *********************/
+template<class pqVar>
+void pqTest(pqVar& A, pqVar& B, pqVar& C) {
+
+    cout << "========== PRIORITY QUEUE STL TESTING ==========\n" << endl;
+    
+    // Create first priority queue and insert 3 elements by priority.
+    priority_queue<pqVar> pq1;
+    pq1.push(C);
+    pq1.push(A);
+    pq1.push(B);
+    cout << "Order in which push happens does not matter for PQ. Top element:\n" << pq1.top() << endl;
+    
+    // Make pq2 a copy of the first priority queue pq1.
+    cout << "Making a copy of PQ 1." << endl;
+    priority_queue<pqVar> pq2(pq1);
+    if( pq1.size() == pq2.size() )
+        cout << "Both PQ 1 and PQ 2 have " << pq1.size() << " elements." << endl;
+    
+    // Pop and display the elements removed by priority.
+    int i;
+    for( i = 0 ; i < 3 ; i++ ) {
+        cout << "Popping element from top of PQ 1:\n" << pq1.top() << endl;
+        pq1.pop();
+    }
+    
+    if( pq1.empty() )
+        cout << "PQ 1 is now empty." << endl;
+    
+    pq1 = pq2;
+    if( !pq1.empty() )
+        cout << "Reassigned PQ 1 to PQ 2's contents. It is no longer empty.\n" << endl;
     
 }
 
@@ -170,23 +226,28 @@ void mapTest(mapVar& A, mapVar& B) {
 int main() {
     
     // Give values to the types we'll use in the STL tests
-    Matrix Mat1, Mat2(1);
-    string str1 = "ABC", str2 = "DEF";
-    char char1 = 'G', char2 = 'H';
-    int int1 = 2, int2 = 3;
+    Matrix Mat1, Mat2(1), Mat3(2);
+    string str1 = "ABC", str2 = "DEF", str3 = "GHI";
+    char char1 = 'J', char2 = 'K', char3 = 'L';
+    int int1 = 2, int2 = 4, int3 = 6;
 
     // Stack STL testing
-    stackTest(Mat1,Mat2);
-    stackTest(str1,str2);
-    stackTest(char1,char2);
-    stackTest(int1,int2);
+    stackTest(Mat1,Mat2,Mat3);
+    stackTest(str1,str2,str3);
+    stackTest(char1,char2,char3);
+    stackTest(int1,int2,int3);
     
     // Map STL testing
-    mapTest(Mat1,Mat2);
-    mapTest(str1,str2);
-    mapTest(char1,char2);
-    mapTest(int1,int2);
+    mapTest(Mat1,Mat2,Mat3);
+    mapTest(str1,str2,str3);
+    mapTest(char1,char2,char3);
+    mapTest(int1,int2,int3);
     
+    // Priority Queue STL testing
+    pqTest(Mat1,Mat2,Mat3);
+    pqTest(str1,str2,str3);
+    pqTest(char1,char2,char3);
+    pqTest(int1,int2,int3);    
 
                     
 
